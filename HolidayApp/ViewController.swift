9bc39e628ff0c3c446b87c6a0e774843ddbb4335
enqq <enqqq@icloud.com>
2022-08-19 17:58:03 +0200

    Initial Commit

---
+++ HolidayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HolidayApp
+//
+//  Created by Dawid Karpiński on 19/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
